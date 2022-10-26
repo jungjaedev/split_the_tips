f51789134ddae63890cfdcd269d5ccb50f557b21
Jaewon <jungjaedev@gmail.com>
2022-10-26 20:34:37 +0900

    Add: setup with typescript

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'babel-plugin-styled-components',
+      [
+        'module-resolver',
+        {
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@': './src',
+            '@data': './src/data',
+            '@components': './src/components',
+            '@styles': './src/styles',
+          },
+        },
+      ],
+    ],
   };
 };
